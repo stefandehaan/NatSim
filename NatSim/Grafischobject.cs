19eeae170b5091a9f047f31876e7e54d5fa933cd
stefandehaan <dastepd@gmail.com>
2019-03-11 10:17:43 +0100

    Chapter 8.1

---
+++ NatSim/Grafischobject.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.
+
+namespace NatSim
+{
+    public class Grafischobject
+    {
+        public Grafischobject()
+        {
+            initClass();
+        }
+
+        public Color Kleur { get; set; }
+        public Color KaderKleur { get; set; }
+        public Point Locatie 
+        {
+            get { return Tekengebied.Locatie; }
+            set { Tekengebied.Locatie = value; }
+        }
+        public Rechthoek Tekengebied { get; set; }
+        public Color Wiskleur { get; set; }
+        public Rechthoek GraphicsVenster { get { return _graphicsVenster; } }
+        public Guid ID { get { return _id; } }
+        private Guid _id;
+        private Rechthoek _graphicsVenster;
+        private bool _verwijderd = false;
+        private Graphics _papier;
+
+        private void initClass()
+        {
+            KaderKleur = Color.Black;
+            Kleur = Color.WhiteSmoke;
+            Wiskleur = Color.PaleGoldenrod;
+
+            Tekengebied = new Rechthoek();
+
+            _id = new Guid.NewGuid();
+        }
+
+        public void Verwijder()
+        {
+            _verwijderd = true;
+            Wis();
+        }
+        public void Wis()
+        {
+            Color oudeKleur = Kleur;
+            Color oudeKaderKleur = KaderKleur;
+            Kleur = Wiskleur;
+            KaderKleur = Wiskleur;
+            Teken();
+            KaderKleur = oudeKaderKleur;
+            Kleur = oudeKleur;
+        }
+
+  
+    }
+}
---
+++ NatSim/Rechthoek.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NatSimII;
+
+namespace NatSim
+{
+    class Rechthoek
+    {
+        public Rechthoek() { }
+        public Rechthoek(Point locatie, Size afmetingen)
+        {
+            Locatie = locatie;
+            Afmetingen = afmetingen;
+        }
+
+        public Size Afmetingen { get; set; }
+        public Point A { get; set; }
+        public Point B { get; set; }
+        public Point C { get; set; }
+        public Point D { get; set; }
+
+        public Point Locatie {
+            get {
+                return A;
+            }
+            set {
+                A = value;
+            }
+        }
+        public int Breedte { get { return Afmetingen.Width; } }
+        public int Hoogte { get { return Afmetingen.Height; } }
+
+        public static Vlak GrensBereikt(Rechthoek rechthoek1, Rechthoek rechthoek2)
+        {
+            Vlak vlak = Vlak.Geen;
+
+            if (rechthoek1.A.X <= rechthoek2.A.X || rechthoek1.A.X >= rechthoek2.A.X)
+            {
+                vlak = Vlak.Verticaal;
+            }
+
+            if (rechthoek1.A.Y <= rechthoek2.A.Y || rechthoek1.A.Y >= rechthoek2.A.Y)
+            {
+                if (vlak == Vlak.Verticaal)
+                {
+                    vlak = Vlak.Hoek;
+                }
+                else
+                {
+                    vlak = Vlak.Horizontaal;
+                }
+            }
+            return vlak;
+        }
+
+        public Vlak GrensBereikt(Rechthoek rechthoek2)
+        {
+            return GrensBereikt(this, rechthoek2);
+        }
+
+        public bool Overlap(Rechthoek rechthoek)
+        {
+            return ((rechthoek.D.X >= A.X && rechthoek.A.X <= D.X) &&
+                (rechthoek.D.Y >= A.Y && rechthoek.A.Y <= D.Y));
+        }
+
+        public Rectangle ToRectangle()
+        {
+            return new Rectangle(Locatie, Afmetingen);
+        }
+
+        public int Oppervlak()
+        {
+            return Breedte * Hoogte;
+        }
+
+        public int Omtrek()
+        {
+            return 2 * (Breedte * Hoogte);
+        }
+
+    }
+}
